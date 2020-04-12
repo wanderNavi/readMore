b7d312848dbfdcc548d4413734f4a96269e1c344
wanderNavi <63549015+wanderNavi@users.noreply.github.com>
2020-04-12 11:05:14 -0400

    webserver start
    created basic set up for webserver page, only have landing page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # readMore
 for The Read More
+
+Eventually will have code for hypothetical collection of essays titled under "The Read More"
---
+++ webserver.py
@@ -0,0 +1,23 @@
+# TEST HYPOTHETICAL HTML WEBSERVER PAGE
+
+############# IMPORTS#############
+# LIBRARIES
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+
+############# GLOBAL VARIABLES #############
+# CONNECT TO DATABASE
+
+############# METHODS #############
+
+############# PAGES #############
+app = Flask(__name__)
+
+# landing page
+@app.route('/')
+def home():
+    page = landing.main()
+    return page
+
+###############################
+app.run(host='0.0.0.0', port=5000, debug=True)
