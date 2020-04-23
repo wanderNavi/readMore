9386b9f77429c8a68e2fb8445695a5f507ed0e7a
wanderNavi <63549015+wanderNavi@users.noreply.github.com>
2020-04-22 23:22:19 -0400

    Update db.py

--- flask-tutorial/flaskr/db.py
+++ flask-tutorial/flaskr/db.py
@@ -15,6 +15,7 @@ def get_db():
 		)
 		g.db.row_factory = sqlite3.Row
 
+	# returning database connection
 	return g.db
 
 def close_db(e=None):
@@ -28,14 +29,24 @@ def close_db(e=None):
 def init_db():
 	db = get_db()
 
+	# open_resource() opens file relative to flaskr package
 	with current_app.open_resource('schema.sql') as f:
 		db.executescript(f.read().decode('utf8'))
 	# end of method
 
+# defines command line command that calls init_db()
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
 	"""Clear the existing data and create new tables."""
 	init_db()
 	click.echo('Initialized the database.')
+	# end of method
+
+# registuring close_db() and init_db() with application instance
+def init_app(app):
+	# call function when cleaning up after retun response
+	app.teardown_appcontext(close_db)
+	# adds new command that can be called with flask command
+	app.cli.add_command(init_db_command)
 	# end of method
