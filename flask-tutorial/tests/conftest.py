01fc8b5c19f959b381ff0d2a9dfaf8f9557a6e44
wanderNavi <63549015+wanderNavi@users.noreply.github.com>
2020-04-24 17:35:41 -0400

    Update conftest.py

--- flask-tutorial/tests/conftest.py
+++ flask-tutorial/tests/conftest.py
@@ -8,6 +8,19 @@ import pytest
 from flaskr import create_app
 from flaskr.db import get_db, init_db
 
+class AuthActions(object):
+	def __init__(self, client):
+		self._client = client
+
+	def login(self, username='test', password='test'):
+		return self._client.post(
+			'/auth/login',
+			data={'username': username, 'password': password}
+		)
+
+	def logout(self):
+		return self._client.get('/auth/logout')
+
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
 	_data_sql = f.read().decode('utf8')
 
@@ -39,3 +52,7 @@ def client(app):
 def runner(app):
 	# creates runner that can call Click commands registered with app
 	return app.test_cli_runner()
+
+@pytest.fixture
+def auth(client):
+	return AuthActions(client)
